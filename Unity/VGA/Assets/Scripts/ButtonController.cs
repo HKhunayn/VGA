7fa8913aa4887a5738aff9b7e7c7819fb8236cb8
Hisham khunayn <hisham.khunayn@gmail.com>
2023-04-09 12:07:11 +0300

    basic UI and pics

---
+++ Unity/VGA/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField]List<Image> childs; 
+
+    public void setSelected(Image m)
+    {
+        foreach (Image im in childs)
+           im.enabled = true; // enable the button image for other childs
+        m.enabled = false;
+    }
+}
