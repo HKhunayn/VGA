078f2e95754a184c586edf19ab0dc2d847852a8e
Hisham khunayn <hisham.khunayn@gmail.com>
2023-04-19 17:17:53 +0300

    node and camera zooming system
    -add node class and its prefab
    -add cameraController which support touch zooming
    -

---
+++ Unity/VGA/Assets/Scripts/camMovementController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class camMovementController : MonoBehaviour
+{
+    Camera cam;
+    [SerializeField]float min = 5;
+    [SerializeField] float max = 15;
+    [SerializeField] float touchZoomMultiplayer = 0.5f;
+
+    float[] lastKnownYPos;
+    private void Start()
+    {
+        cam= Camera.main;
+        lastKnownYPos = new float[2]; // save the touch 0 and touch 1 
+    }
+
+    private void Update()
+    {
+        if (Input.mouseScrollDelta.y != 0) // for mouse zoom
+            changeZoom(Input.mouseScrollDelta.y);
+
+
+        else if (Input.touchCount > 1) // for touch zoom
+        {
+            float lastDis = Mathf.Abs(lastKnownYPos[0] - lastKnownYPos[1]) ;
+            float crntDis = Mathf.Abs(Input.GetTouch(0).position.y - Input.GetTouch(1).deltaPosition.y);
+            if (lastDis != crntDis) // zoom out or zoom out
+                changeZoom(touchZoomMultiplayer*Mathf.Clamp(lastDis - crntDis,-1,1));
+
+           
+            lastKnownYPos[0] = Input.GetTouch(0).position.y; lastKnownYPos[1]= Input.GetTouch(1).position.y;
+        }
+            
+    }
+    void changeZoom(float delta)
+    {
+        Debug.Log(delta+"");
+        cam.orthographicSize=Mathf.Clamp(cam.orthographicSize-= delta, min, max);
+    }
+
+    void changePos(Vector3 delta)
+    {
+        cam.transform.position += delta;
+    }
+
+}
+
+
---
+++ Unity/VGA/Assets/Scripts/node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class node : MonoBehaviour
+{
+
+    private int ID;
+    [SerializeField]private Color color;
+    List<node> neighbors;
+
+    void Start()
+    {
+        neighbors = new List<node>();
+    }
+
+    public void setID(int N) {ID = N;}
+    public int getID() { return ID;}
+    public void setNeighbors(List<node> l) { neighbors = l; }
+    public List<node> getNeighbors() { return neighbors; }
+    public void addNeighbor(node n) { neighbors.Add(n); }
+    public void removeNeighbor(node n) { try { neighbors.Remove(n); } catch { } }
+
+    public void sePos(Vector3 v) { transform.position = v; }
+    public Vector3 getPos() { return transform.position; }
+
+    public void changeColor(Color color) { this.color = color; }
+
+}
